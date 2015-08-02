7c40f078ac1ffa05a56a2cf68da530ee44898000
alessgrdi <alejandrogrageradiaz@hotmail.com>
2015-08-03 00:26:10 +0200

    Despliegue1

---
+++ controllers/quiz_controller.js
@@ -0,0 +1,97 @@
+var models = require ('../models/models.js');
+
+// GET /quizes/question
+/*exports.question = function(req, res) {
+  models.Quiz.findAll().success(function(quiz) {
+   res.render('quizes/question', {pregunta: quiz[0].pregunta});
+ })
+};*/
+
+// GET /quizes/answer
+/*exports.answer = function(req, res) {
+models.Quiz.findAll().success(function(quiz) {
+   if (req.query.respuesta === quiz[0].respuesta){
+      res.render('quizes/answer', {respuesta: 'Correcto'});
+   } else {
+      res.render('quizes/answer', {respuesta: 'Incorrecto'});
+   }
+  })
+};*/
+
+exports.author = function(req, res) {
+  res.render('author');
+};
+
+// POST /quizes/create
+exports.create = function(req, res) {
+/*  req.body.quiz.UserId = req.session.user.id;
+  if(req.files.image){
+    req.body.quiz.image = req.files.image.name;
+  }*/
+  var quiz = models.Quiz.build( req.body.quiz );
+
+  /*quiz
+  .validate()
+  .then(
+    function(err){
+      if (err) {
+        res.render('quizes/new', {quiz: quiz, errors: err.errors});
+      } else {*/
+         // save: guarda en DB campos pregunta y respuesta de quiz
+        quiz.save({fields: ["pregunta", "respuesta"]}).then( function(){ 
+          res.redirect('/quizes');
+        })
+      //}      // res.redirect: Redirección HTTP a lista de preguntas
+    //}
+  //).catch(function(error){next(error)});
+};
+
+// GET /quizes/:id
+exports.show = function(req, res) {
+  models.Quiz.find(req.params.quizId).then(function(quiz) {
+  res.render('quizes/show', { quiz: quiz});
+})
+};
+
+// GET /quizes/:id/answer
+exports.answer = function(req, res) {
+  models.Quiz.find(req.params.quizId).then(function(quiz) {
+  //var resultado = 'Incorrecto';
+  /*if (req.query.respuesta === req.quiz.respuesta) {
+    resultado = 'Correcto';
+  }
+  res.render(
+    'quizes/answer',
+    { quiz: req.quiz,
+      respuesta: resultado,
+      errors: []
+    }*/
+    if (req.query.respuesta === quiz.respuesta){
+       res.render('quizes/answer', {quiz: quiz, respuesta: 'Correcto'});
+    } else {
+       res.render('quizes/answer', {quiz: quiz, respuesta: 'Incorrecto'});
+    }
+  }) //);
+};
+
+exports.index = function(req, res) {
+  /*var options = {};
+  if(req.user){
+    options.where = {UserId: req.user.id}
+  }*/
+
+  //models.Quiz.findAll(options).then(
+  models.Quiz.findAll().then(
+    function(quizes) {
+      res.render('quizes/index.ejs', {quizes: quizes});//, errors: []});
+    })
+  //).catch(function(error){next(error)});
+};
+
+// GET /quizes/new
+exports.new = function(req, res) {
+  var quiz = models.Quiz.build( // crea objeto quiz
+    {pregunta: "Pregunta", respuesta: "Respuesta"}
+  );
+  res.render('quizes/new', {quiz: quiz});//, errors: []});
+};
---
+++ models/quiz.js
@@ -0,0 +1,16 @@
+//Definicion del modelo de quiz
+
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define(
+    'Quiz',
+  { pregunta: {
+      type: DataTypes.STRING,
+      validate: { notEmpty: {msg: "-> Falta Pregunta"}}
+    },
+    respuesta: {
+      type: DataTypes.STRING,
+      validate: { notEmpty: {msg: "-> Falta Respuesta"}}
+    }
+   }
+  );
+}
